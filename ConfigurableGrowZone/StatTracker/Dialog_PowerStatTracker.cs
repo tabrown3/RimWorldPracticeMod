525ee2b0115b56ac7fd67b04fe240d9b4c25fea8
t-a-brown@hotmail.com <t-a-brown@hotmail.com>
2020-03-10 22:41:11 +0800

    Dialog now draws rectangles to form a graph with zero centered in the middle, and scaling

--- ConfigurableGrowZone/StatTracker/Dialog_PowerStatTracker.cs
+++ ConfigurableGrowZone/StatTracker/Dialog_PowerStatTracker.cs
@@ -11,6 +11,8 @@ namespace ConfigurableGrowZone
     {
         private Dictionary<int, DataPoint> history;
 
+        float barWidth = 10f;
+
         public Dialog_PowerStatTracker(Dictionary<int, DataPoint> history)
         {
             this.history = history;
@@ -19,20 +21,50 @@ namespace ConfigurableGrowZone
             closeOnClickedOutside = true;
             absorbInputAroundWindow = true;
             draggable = true;
-            forcePause = true;
+            forcePause = false;
         }
 
         public override void DoWindowContents(Rect inRect)
         {
             Text.Font = GameFont.Small;
             Rect outRect = new Rect(inRect);
-            outRect.yMin += 20f;
-            outRect.yMax -= 40f;
+            //outRect.yMin += 20f;
+            //outRect.yMax -= 40f;
             outRect.width -= 16f;
 
-            float curY = 0f;
+            List<DataPoint> historyList = history.Select(u => u.Value).OrderByDescending(u => u.TimeStamp).ToList();
 
-            //throw new NotImplementedException();
+            int barCount = Mathf.Min(Mathf.FloorToInt(outRect.width / barWidth), historyList.Count);
+            float maxValue = historyList.Max(u => u.DigestValue);
+            float minValue = historyList.Min(u => u.DigestValue);
+
+            float roofValue = Mathf.Max(maxValue, 0);
+            float basementValue = Mathf.Min(minValue, 0);
+
+            float scaleDivisor = Mathf.Max(roofValue, Mathf.Abs(basementValue));
+
+            for (int i = 0; i < barCount; i++)
+            {
+                DataPoint point = historyList[i];
+
+                float xPos = outRect.width - (i + 1) * barWidth;
+
+                float barHeight;
+                if (scaleDivisor != 0) // wouldn't want to divide by zero, would we...
+                {
+                    barHeight = (point.DigestValue / scaleDivisor) * outRect.height/2;
+                }
+                else
+                {
+                    barHeight = 0; // scaleDivisor will only be zero if all values are zero
+                }
+
+                float yPos = outRect.height/2 - barHeight;
+                Rect barRect = new Rect(xPos, yPos, barWidth, barHeight);
+                Widgets.DrawBoxSolid(barRect, Color.yellow);
+            }
+
+            Widgets.DrawLine(new Vector2(0, outRect.height / 2), new Vector2(outRect.width, outRect.height / 2), Color.white, 1f);
         }
     }
 }
