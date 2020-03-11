5c20490e1b17a12405f2fc6aac8be93646b656fe
t-a-brown@hotmail.com <t-a-brown@hotmail.com>
2020-03-11 12:05:53 +0800

    GUI.BeginGroup seems to be working well for drawing the graph

--- ConfigurableGrowZone/StatTracker/Dialog_PowerStatTracker.cs
+++ ConfigurableGrowZone/StatTracker/Dialog_PowerStatTracker.cs
@@ -26,13 +26,14 @@ namespace ConfigurableGrowZone
 
         public override void DoWindowContents(Rect inRect)
         {
+            GUI.BeginGroup(inRect);
+            inRect = inRect.AtZero();
+
             Text.Font = GameFont.Small;
 
             float curY = 0f;
 
-            Rect titleRect = new Rect(inRect);
-            titleRect.y = curY;
-            titleRect.height = 32f;
+            Rect titleRect = new Rect((inRect.width / 2) - (Text.CalcSize(metricKey).x / 2), curY, inRect.width, 32f);
             Widgets.Label(titleRect, metricKey);
             curY += titleRect.height;
 
@@ -41,17 +42,28 @@ namespace ConfigurableGrowZone
             graphRect.y = curY;
             graphRect.height -= 64f;
             //graphRect.width -= 16f;
-            //GUI.BeginGroup(graphRect)
+            GUI.BeginGroup(graphRect);
+            graphRect = graphRect.AtZero();
+
+            DrawGraph(graphRect);
+
+            GUI.EndGroup(); // graphRect end
             curY += graphRect.height;
 
+            GUI.EndGroup(); // inRect end
+        }
+
+        private void DrawGraph(Rect graphRect)
+        {
+            // The presentation layer should determine how data is presented, so I think it's ok to leave this logic here
             List<DataPoint> historyList = history.Select(u => u.Value).OrderByDescending(u => u.TimeStamp).ToList();
 
             int barCount = Mathf.Min(Mathf.FloorToInt(graphRect.width / barWidth), historyList.Count);
             float maxValue = historyList.Max(u => u.DigestValue);
             float minValue = historyList.Min(u => u.DigestValue);
 
-            float roofValue = Mathf.Max(maxValue, 0);
-            float basementValue = Mathf.Min(minValue, 0);
+            float roofValue = Mathf.Max(maxValue, 0f);
+            float basementValue = Mathf.Min(minValue, 0f);
 
             float scaleDivisor = Mathf.Max(roofValue, Mathf.Abs(basementValue));
 
@@ -64,21 +76,21 @@ namespace ConfigurableGrowZone
                 float barHeight;
                 if (scaleDivisor != 0) // wouldn't want to divide by zero, would we...
                 {
-                    barHeight = (point.DigestValue / scaleDivisor) * graphRect.height/2;
+                    barHeight = (point.DigestValue / scaleDivisor) * graphRect.height / 2;
                 }
                 else
                 {
                     barHeight = 0; // scaleDivisor will only be zero if all values are zero
                 }
 
-                float yPos = (graphRect.y + graphRect.height/2) - barHeight;
+                float yPos = graphRect.height / 2 - barHeight;
                 Rect barRect = new Rect(xPos, yPos, barWidth, barHeight);
                 Widgets.DrawBoxSolid(barRect, Color.yellow);
             }
 
-            Widgets.DrawLine(new Vector2(0, graphRect.y), new Vector2(graphRect.width, graphRect.y), Color.white, 1f); // chart top
-            Widgets.DrawLine(new Vector2(0, graphRect.y + graphRect.height / 2), new Vector2(graphRect.width, graphRect.y + graphRect.height / 2), Color.white, 1f); // chart zero
-            Widgets.DrawLine(new Vector2(0, graphRect.y + graphRect.height), new Vector2(graphRect.width, graphRect.y + graphRect.height), Color.white, 1f); // chart bottom
+            Widgets.DrawLine(new Vector2(0f, 0f), new Vector2(graphRect.width, 0f), Color.white, 1f); // chart top
+            Widgets.DrawLine(new Vector2(0f, graphRect.height / 2), new Vector2(graphRect.width, graphRect.height / 2), Color.white, 1f); // chart zero
+            Widgets.DrawLine(new Vector2(0f, graphRect.height - 1f), new Vector2(graphRect.width, graphRect.height - 1f), Color.white, 1f); // chart bottom
         }
     }
 }
