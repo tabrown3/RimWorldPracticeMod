5798f3b0e900a3fdfef850d804707756f1362315
t-a-brown@hotmail.com <t-a-brown@hotmail.com>
2020-03-19 22:25:10 +0800

    Created interface and base classes for unary and binary operators

---
+++ ConfigurableGrowZone/StatTracker/Operator/BinaryOperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurableGrowZone
+{
+    public abstract class BinaryOperator<T> : IOperator<T>
+    {
+        public T First { get; set; }
+        public T Second { get; set; }
+
+        public abstract T Call();
+    }
+}
---
+++ ConfigurableGrowZone/StatTracker/Operator/UnaryOperator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurableGrowZone
+{
+    public abstract class UnaryOperator<T> : IOperator<T>
+    {
+        public T First { get; set; }
+        public abstract T Call();
+    }
+}
