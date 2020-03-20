5b6736217e4febfe44ce4f3784c5e01c72a94abb
t-a-brown@hotmail.com <t-a-brown@hotmail.com>
2020-03-20 13:56:59 +0800

    Created Negate and Plus Operators

---
+++ ConfigurableGrowZone/StatTracker/Operator/PlusOperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurableGrowZone
+{
+    public class PlusOperator : BinaryOperator<float>
+    {
+        public override float Call()
+        {
+            return First + Second;
+        }
+    }
+}
