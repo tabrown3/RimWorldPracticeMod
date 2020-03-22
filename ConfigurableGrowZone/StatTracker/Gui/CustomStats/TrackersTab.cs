8aa97d0ac489d216f11cbdbd379d6a1faff9995f
t-a-brown@hotmail.com <t-a-brown@hotmail.com>
2020-03-22 23:34:26 +0800

    Revert "Added UniRx for trial and evaluation"
    This reverts commit 12892dc5050397b7238e25211f068fa8e29deced.

--- ConfigurableGrowZone/StatTracker/Gui/CustomStats/TrackersTab.cs
+++ ConfigurableGrowZone/StatTracker/Gui/CustomStats/TrackersTab.cs
@@ -11,20 +11,10 @@ namespace ConfigurableGrowZone
     public class TrackersTab : ITabDrawable<CustomStatsTab>
     {
         public CustomStatsTab TabType => CustomStatsTab.Trackers;
-        private readonly MapStatTracker statTracker;
-        public TrackersTab()
-        {
-            statTracker = Find.CurrentMap.GetComponent<MapStatTracker>();
-        }
 
         public void DrawTab(Rect pane)
         {
             Widgets.DrawBoxSolid(pane, Color.red);
-
-            foreach(var trackerComp in statTracker.TrackerComps)
-            {
-                //trackerComp.Name
-            }
         }
     }
 }
