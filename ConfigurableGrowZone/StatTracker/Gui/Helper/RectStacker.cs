181d179cd429dd22e9ba68c444c2160becc859c0
t-a-brown@hotmail.com <t-a-brown@hotmail.com>
2020-03-27 18:37:54 +0800

    Attempting to create RectStacker

---
+++ ConfigurableGrowZone/StatTracker/Gui/Helper/RectStacker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ConfigurableGrowZone
+{
+    public class RectStacker
+    {
+        private float curY = 0f;
+        private float curHeight = 0f;
+        public RectStacker(Rect outerRect)
+        {
+            curY = outerRect.y;
+            curHeight = outerRect.height;
+        }
+
+        public RectStacker Then(Func<Rect, Rect> thenFunc)
+        {
+            var newRect = new Rect();
+            newRect.y = this.curY;
+            var bob = thenFunc(newRect);
+
+            curY += bob.y + bob.height;
+            curHeight += bob.y + bob.height;
+
+            return this;
+        }
+
+        public RectStacker Then(RectStacker rectStacker)
+        {
+            curY += rectStacker.curHeight;
+            curHeight += rectStacker.curHeight;
+            return this;
+        }
+    }
+}
