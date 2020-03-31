41eb36af236bef47716ad0e1ac57e83a35605609
t-a-brown@hotmail.com <t-a-brown@hotmail.com>
2020-03-31 14:25:04 +0800

    New row now created when all rows are valid

--- ConfigurableGrowZone/StatTracker/Gui/CustomStats/AddMetric/Derived/AddOperatorListComponent.cs
+++ ConfigurableGrowZone/StatTracker/Gui/CustomStats/AddMetric/Derived/AddOperatorListComponent.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniRx;
 using UnityEngine;
+using Verse;
 
 namespace ConfigurableGrowZone
 {
     public class AddOperatorListComponent
     {
         private readonly List<string> allTrackerNames;
-
         private readonly List<AddOperatorRowComponent> rows = new List<AddOperatorRowComponent>();
+        //private AddOperatorRowComponent bottomRow => rows.Count > 0 ? rows[rows.Count - 1] : null;
+        private IObservable<bool> rowsBecameValid;
 
         public List<Type> Operators { get; } = new List<Type>();
         public List<SourceMetric> SourceMetrics { get; } = new List<SourceMetric>();
@@ -20,7 +23,7 @@ namespace ConfigurableGrowZone
         {
             allTrackerNames = allSourceMetrics.Select(u => u.ParentName).Distinct().ToList();
 
-            rows.Add(new AddOperatorRowComponent(allOperatorTypes, allTrackerNames, allSourceMetrics));
+            AddRow(allOperatorTypes, allTrackerNames, allSourceMetrics);
         }
 
         public Rect Draw(Rect inRect)
@@ -37,6 +40,36 @@ namespace ConfigurableGrowZone
                         .GetRect();
                 })
                 .ThenForEach(rows, (u, row, ind) => row.Draw(u)).GetRect();
+        }
+
+        private void AddRow(List<Type> allOperatorTypes, List<string> allTrackerNames, List<SourceMetric> allSourceMetrics)
+        {
+            var newRow = new AddOperatorRowComponent(allOperatorTypes, allTrackerNames, allSourceMetrics);
+            rows.Add(newRow);
+
+            rowsBecameValid = RowsBecameValidFactory();
+            rowsBecameValid.Subscribe(u =>
+            {
+                AddRow(allOperatorTypes, allTrackerNames, allSourceMetrics);
+                Log.Message("Rows all valid!");
+            });
+        }
+
+        private IObservable<bool> RowsBecameValidFactory()
+        {
+            bool lastValidityState = false; // can start false because after a new row is added, it will always be invalid
+
+            // observable that emits when the list's rows go from "not all being valid" to "all being valid"
+            return rows.ToObservable()
+                .SelectMany(u => u.RowBecameValid)
+                .Select(u => rows.All(v => v.IsValid()))
+                .Where(curValidityState =>
+                {
+                    bool validityStateChanged = curValidityState != lastValidityState;
+                    lastValidityState = curValidityState;
+                    return validityStateChanged && curValidityState;
+                }
+            );
         }
     }
 }
--- ConfigurableGrowZone/StatTracker/Gui/CustomStats/AddMetric/Derived/AddOperatorRowComponent.cs
+++ ConfigurableGrowZone/StatTracker/Gui/CustomStats/AddMetric/Derived/AddOperatorRowComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniRx;
 using UnityEngine;
 
 namespace ConfigurableGrowZone
@@ -12,52 +13,104 @@ namespace ConfigurableGrowZone
         private readonly List<Type> allOperatorTypes;
         private readonly List<string> allTrackerNames;
         private readonly List<SourceMetric> allSourceMetrics;
+        private List<SourceMetric> availableMetrics = new List<SourceMetric>();
 
         private Type chosenOperator = null;
         private string chosenTrackerName = "";
         private SourceMetric chosenSourceMetric = null;
+
         private bool chosenOperatorIsBinary = false;
 
-        private List<SourceMetric> availableMetrics = new List<SourceMetric>();
+        private readonly Subject<Type> operatorChosen = new Subject<Type>();
+        private readonly Subject<string> trackerNameChosen = new Subject<string>();
+        private readonly Subject<SourceMetric> sourceMetricChosen = new Subject<SourceMetric>();
+
+        public readonly IObservable<bool> RowBecameValid;
+
         public AddOperatorRowComponent(List<Type> allOperatorTypes, List<string> allTrackerNames, List<SourceMetric> allSourceMetrics)
         {
             this.allOperatorTypes = allOperatorTypes;
             this.allTrackerNames = allTrackerNames;
             this.allSourceMetrics = allSourceMetrics;
+
+            RowBecameValid = RowBecameValidFactory();
         }
 
         public Rect Draw(Rect inRect)
         {
             return new RectSpanner(inRect)
                 .Then(
-                    u => StatWidgets.DrawTextButtonBottomLabel(u, "Operator", allOperatorTypes, v => v.Name, chosenOperator,
-                        v => {
-                            chosenOperator = v;
-                            chosenTrackerName = "";
-                            chosenSourceMetric = null;
-                            chosenOperatorIsBinary = !StatTypesHelper.IsUnaryOperator(chosenOperator);
-                        })
+                    u => StatWidgets.DrawTextButtonBottomLabel(u, "Operator", allOperatorTypes, v => v.Name, chosenOperator, OperatorChosen)
                 )
                 .ThenGap(50f)
                 .IfThen(
                     () => chosenOperator != null && chosenOperatorIsBinary,
-                    u => StatWidgets.DrawTextButtonBottomLabel(u, "Tracker", allTrackerNames, v => v, chosenTrackerName,
-                        v => {
-                            chosenTrackerName = v;
-                            chosenSourceMetric = null;
-                            availableMetrics = allSourceMetrics.Where(w => w.ParentName == v).ToList();
-                        })
+                    u => StatWidgets.DrawTextButtonBottomLabel(u, "Tracker", allTrackerNames, v => v, chosenTrackerName, TrackerNameChosen)
                 )
                 .ThenGap(50f)
                 .IfThen(
                     () => !string.IsNullOrEmpty(chosenTrackerName),
-                    u => StatWidgets.DrawTextButtonBottomLabel(u, "Metric", availableMetrics, v => v.Name, chosenSourceMetric, v => chosenSourceMetric = v)
+                    u => StatWidgets.DrawTextButtonBottomLabel(u, "Metric", availableMetrics, v => v.Name, chosenSourceMetric, SourceMetricChosen)
                 ).GetRect();
         }
 
         public bool IsValid()
         {
-            throw new NotImplementedException();
+            bool hasOperator = chosenOperator != null;
+
+            bool isValid;
+            if (hasOperator && chosenOperatorIsBinary)
+            {
+                isValid = !string.IsNullOrEmpty(chosenTrackerName) && chosenSourceMetric != null;
+            }
+            else
+            {
+                isValid = hasOperator;
+            }
+
+            return isValid;
+        }
+
+        private void OperatorChosen(Type chosenOperator)
+        {
+            this.chosenOperator = chosenOperator;
+            chosenTrackerName = "";
+            chosenSourceMetric = null;
+            chosenOperatorIsBinary = !StatTypesHelper.IsUnaryOperator(chosenOperator);
+            operatorChosen.OnNext(chosenOperator);
+        }
+
+        private void TrackerNameChosen(string chosenTrackerName)
+        {
+            this.chosenTrackerName = chosenTrackerName;
+            chosenSourceMetric = null;
+            availableMetrics = allSourceMetrics.Where(w => w.ParentName == chosenTrackerName).ToList();
+            trackerNameChosen.OnNext(chosenTrackerName);
+        }
+
+        private void SourceMetricChosen(SourceMetric chosenSourceMetric)
+        {
+            this.chosenSourceMetric = chosenSourceMetric;
+            sourceMetricChosen.OnNext(chosenSourceMetric);
+        }
+
+        private IObservable<bool> RowBecameValidFactory()
+        {
+            bool lastValidityState = false;
+
+            // observable that emits when rows's validity changes from invalid to valid
+            return Observable.Merge(
+                    operatorChosen.Select(u => IsValid()),
+                    trackerNameChosen.Select(u => IsValid()),
+                    sourceMetricChosen.Select(u => IsValid())
+                )
+                .Where(curValidityState =>
+                    {
+                        bool validityStateChanged = curValidityState != lastValidityState;
+                        lastValidityState = curValidityState;
+                        return validityStateChanged && curValidityState;
+                    }
+                );
         }
     }
 }
